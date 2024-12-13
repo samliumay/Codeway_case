016e7a5d480287a01e6c51868e168a23368a7595
samliumay <samliumay965@samliumay.com>
2024-12-13 06:58:46 +0300

    Dockerization of the project is finished

--- bezkoder-app/server.js
+++ bezkoder-app/server.js
@@ -22,9 +22,9 @@ app.get("/", (req, res) => {
 });
 
 require("./app/routes/tutorial.routes.js")(app);
-
+require("dotenv").config();
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
